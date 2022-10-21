bf203ab3e7d00eb6bba36462ae04d0cbddf2d952
Juampe López <juampe.lopez@gmail.com>
2022-10-21 02:02:06 +0200

    Cleanup.

--- main.c
+++ main.c
@@ -25,13 +25,27 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+#define LED_PIN PICO_DEFAULT_LED_PIN
 
 static volatile int gps_raw_end = false;
 static volatile int gps_print_available = false;
 static int gps_chars_rx = 0;
-static char gps_raw_data[80];
-static char gps_print_data[80];
+
+#define BUF_SIZE 100
+static char gps_raw_data[BUF_SIZE];
+static char gps_print_data[BUF_SIZE];
+
+enum gps_fields {GPS_ID = 0,
+                GPS_TIME,
+                GPS_LATITUDE,
+                GPS_LATITUDE_NS,
+                GPS_LONGITUDE,
+                GPS_LONGITUDE_WE,
+                GPS_QUALITY,
+                GPS_NUM_SATS,
+                GPS_HDOP,
+                GPS_ALTITUDE,
+                GPS_GEOID_HEIGHT}; 
 
 // RX interrupt handler
 void on_uart_rx() {
@@ -53,9 +67,7 @@ void on_uart_rx() {
     if (gps_raw_end)
     {
         gps_raw_data[gps_chars_rx] = '\0';
-        strncpy(gps_print_data, (const char *)gps_raw_data, 80);
-        //printf(gps_print_data);
-        //printf("\r\n");
+        strncpy(gps_print_data, (const char *)gps_raw_data, BUF_SIZE);
         gps_raw_end = false;
         gps_print_available = true;
     }
@@ -64,8 +76,8 @@ void on_uart_rx() {
 void setup()
 {
     // Clean gps_raw_data
-    memset(gps_raw_data, 0, 80);
-    memset(gps_print_data, 0, 80);
+    memset(gps_raw_data, 0, BUF_SIZE);
+    memset(gps_print_data, 0, BUF_SIZE);
 
     // Setup USB UART
     stdio_usb_init();
@@ -119,8 +131,18 @@ void parse_comma_delimited_str(char *string, char **fields, int max_fields)
 	}
 }
 
+int quick_pow10(int n)
+{
+    static int pow10[10] = {
+        1, 10, 100, 1000, 10000, 
+        100000, 1000000, 10000000, 100000000, 1000000000
+    };
+
+    return pow10[n]; 
+}
+
 int main() {
-    char *field[20];
+    char *gps_field[20];
 
     setup();
 
@@ -128,59 +150,77 @@ int main() {
     {
         if (gps_print_available)
         {
-            if (strncmp(&gps_print_data[3], "GGA", 3) == 0) {
-                parse_comma_delimited_str(gps_print_data, field, 20);
-                //debug_print_fields(i,field);
-                printf("UTC Time  :%s\r\n",field[1]);
-                printf("Latitude  :%s\r\n",field[2]);
-                printf("Longitude :%s\r\n",field[4]);
-                printf("Altitude  :%s\r\n",field[9]);
-                printf("Satellites:%s\r\n",field[7]);
-
-                char *p;
-                int utc_hhmmss, utc_milliseconds, utc_ms_scale;
-
-                if ( (p=strchr(field[1], '.' )) != NULL ) {
-                    sscanf( field[1], "%d.%d", &utc_hhmmss, &utc_milliseconds );
-                    utc_ms_scale = strlen( p+1 );
-                    printf("UTC seconds       :%d\r\n",utc_hhmmss);
-                    printf("UTC milliseconds  :%d\r\n",utc_milliseconds);
-                    printf("UTC scale         :%d\r\n",utc_ms_scale);
-                }
-
-                int lat_DDmm, lat_mm, lat_mm_scale;
-
-                if ( (p=strchr(field[2], '.' )) != NULL ) {
-                    sscanf( field[2], "%d.%d", &lat_DDmm, &lat_mm );
-                    lat_mm_scale = strlen( p+1 );
-                    printf("LAT DDmm     :%d\r\n",lat_DDmm);
-                    printf("LAT mm       :%d\r\n",lat_mm);
-                    printf("LAT mm scale :%d\r\n",lat_mm_scale);
-                }
-
-                int lon_DDmm, lon_mm, lon_mm_scale;
-
-                if ( (p=strchr(field[4], '.' )) != NULL ) {
-                    sscanf( field[4], "%d.%d", &lon_DDmm, &lon_mm );
-                    lat_mm_scale = strlen( p+1 );
-                    printf("LON DDmm     :%d\r\n",lon_DDmm);
-                    printf("LON mm       :%d\r\n",lon_mm);
-                    printf("LON mm scale :%d\r\n",lon_mm_scale);
-                }
-
-                int alt_m, alt_cm, alt_cm_scale;
-
-                if ( (p=strchr(field[9], '.' )) != NULL ) {
-                    sscanf( field[9], "%d.%d", &alt_m, &alt_cm );
-                    lat_mm_scale = strlen( p+1 );
-                    printf("LAT DDmm     :%d\r\n",alt_m);
-                    printf("LAT mm       :%d\r\n",alt_cm);
-                    printf("LAT mm scale :%d\r\n",alt_cm_scale);
-                }
-            }
             gpio_put(LED_PIN, 1);
-            //printf(gps_print_data);
-            //printf("\r\n");
+            if (strncmp(&gps_print_data[3], "GGA", 3) == 0) {
+                parse_comma_delimited_str(gps_print_data, gps_field, 20);
+                
+#ifdef SITRIPIO_DBG
+                printf("UTC Time  :%s\r\n", gps_field[GPS_TIME]);
+                printf("Latitude  :%s\r\n", gps_field[GPS_LATITUDE]);
+                printf("N/S       :%s\r\n", gps_field[GPS_LATITUDE_NS]);
+                printf("Longitude :%s\r\n", gps_field[GPS_LONGITUDE]);
+                printf("E/W       :%s\r\n", gps_field[GPS_LONGITUDE_WE]);
+                printf("Altitude  :%s\r\n", gps_field[GPS_ALTITUDE]);
+                printf("Satellites:%s\r\n", gps_field[GPS_NUM_SATS]);
+#endif
+                char *p;
+                int HHMMSSmmm, utc_hhmmss, utc_milliseconds, utc_ms_scale = 0;
+                int latitude, lat_DDmm, lat_mm, lat_mm_scale = 0;
+                int longitude, lon_DDmm, lon_mm, lon_mm_scale = 0;
+                int altitude, alt_m, alt_cm, alt_cm_scale = 0;
+                int pow_scale;
+
+                if ( (p=strchr(gps_field[GPS_TIME], '.' )) != NULL ) {
+                    sscanf( gps_field[GPS_TIME], "%d.%d", &utc_hhmmss, &utc_milliseconds );
+                    utc_ms_scale = strlen( p+1 );
+
+                    pow_scale = quick_pow10(utc_ms_scale);
+                    HHMMSSmmm = utc_hhmmss*pow_scale + utc_milliseconds;
+
+                    printf("HHMMSSmmm  :%d\r\n", HHMMSSmmm);
+                }
+
+                if ( (p=strchr(gps_field[GPS_LATITUDE], '.' )) != NULL ) {
+                    sscanf( gps_field[GPS_LATITUDE], "%d.%d", &lat_DDmm, &lat_mm );
+                    lat_mm_scale = strlen( p+1 );
+
+                    pow_scale = quick_pow10(lat_mm_scale);
+                    latitude = lat_DDmm*pow_scale + lat_mm;
+
+                    if (gps_field[GPS_LATITUDE_NS][0] == 'S')
+                        latitude = -latitude;
+
+                    printf("LATITUDE  :%d\r\n", latitude);
+                }
+
+                if ( (p=strchr(gps_field[GPS_LONGITUDE], '.' )) != NULL ) {
+                    sscanf( gps_field[GPS_LONGITUDE], "%d.%d", &lon_DDmm, &lon_mm );
+                    lon_mm_scale = strlen( p+1 );
+
+                    pow_scale = quick_pow10(lon_mm_scale);
+                    longitude = lon_DDmm*pow_scale + lon_mm;
+
+                    printf("LONGITUDE :%d\r\n", longitude);
+
+                    if (gps_field[GPS_LONGITUDE_WE][0] == 'W')
+                        longitude = -longitude;
+                }
+
+                if ( (p=strchr(gps_field[GPS_ALTITUDE], '.' )) != NULL ) {
+                    sscanf( gps_field[GPS_ALTITUDE], "%d.%d", &alt_m, &alt_cm );
+                    lat_mm_scale = strlen( p+1 );
+                    // printf("LAT DDmm     :%d\r\n",alt_m);
+                    // printf("LAT mm       :%d\r\n",alt_cm);
+                    // printf("LAT mm scale :%d\r\n",alt_cm_scale);
+
+                    pow_scale = quick_pow10(alt_cm_scale);
+                    altitude = alt_m*pow_scale + alt_cm;
+
+                    printf("ALTITUDE  :%d\r\n", altitude);
+                }
+
+                printf("sizeof(int) = %d\r\n", sizeof(int));
+            }
             gpio_put(LED_PIN, 0);
             gps_print_available = false;
         }
